663c4961869494b9ef3c80913657121ccabbc1a0
deepak5998 <deepakkumbhar1920@gmail.com>
2021-10-07 18:05:12 +0530

    added:code for getting stock account

---
+++ src/com/Day11/StockAccount.java
@@ -0,0 +1,13 @@
+package com.Day11;
+
+public class StockAccount {
+
+}
+
+interface interf (){
+    double valueOf();
+    void buy(int amount,String symbol);
+    void sell(int amount,String symbol);
+    void save(String filename);
+    void printReport();
+}
